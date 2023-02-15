bc83ca470da47b9bfbb5e46ea2f18ad42f20454a
KenjiWorkES <lucas.kenjies@gmail.com>
2023-02-15 14:37:21 -0300

    feat: add sass, css variables and atomic pages

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,41 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
+<script setup></script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <h1>Test</h1>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+<style lang="scss">
+@import url('https://fonts.googleapis.com/css2?family=Outfit:wght@300;400;500;600;700;800&display=swap');
+
+:root {
+  --font-family: 'Outfit', sans-serif;
+  --color-red-400: #fc4747;
+  --color-blue-200: #5a698f;
+  --color-blue-400: #161d2f;
+  --color-blue-600: #10141e;
+  --color-white: #fff;
+
+  --font-size-xl: 3.2rem;
+  --font-size-l: 2.4rem;
+  --font-size-m: 1.8rem;
+  --font-size-s: 1.5rem;
+  --font-size-xs: 1.3rem;
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
+
+*,
+*::before,
+*::after {
+  margin: 0;
+  padding: 0;
+  box-sizing: inherit;
 }
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+
+html {
+  font-size: 62.5%;
+}
+
+body {
+  box-sizing: border-box;
+  font-family: var(--font-family);
 }
 </style>
