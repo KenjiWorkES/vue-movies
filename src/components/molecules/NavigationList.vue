72ccc104054da2b396a2683a2666c1fac6617f3a
KenjiWorkES <lucas.kenjies@gmail.com>
2023-02-15 17:17:27 -0300

    feat: create and divide theHeader in others components

---
+++ src/components/molecules/NavigationList.vue
@@ -0,0 +1,42 @@
+<script setup>
+import NavigationItem from '../atoms/NavigationItem.vue';
+</script>
+
+<template>
+  <nav class="header__nav">
+    <ul class="header__list">
+      <navigation-item icon-id="all" />
+      <navigation-item icon-id="movies" />
+      <navigation-item icon-id="tv" />
+      <navigation-item icon-id="bookmark" />
+    </ul>
+  </nav>
+</template>
+
+<style scoped lang="scss">
+.header {
+  &__nav {
+    width: 100%;
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    @media screen and (min-width: 1024px) {
+      align-items: flex-start;
+    }
+  }
+
+  &__list {
+    list-style: none;
+    display: flex;
+    align-items: center;
+    column-gap: 2.4rem;
+
+    @media screen and (min-width: 1024px) {
+      flex-direction: column;
+      row-gap: 4rem;
+    }
+  }
+}
+</style>
--- src/components/organisms/TheHeader.vue
+++ src/components/organisms/TheHeader.vue
@@ -1,32 +1,13 @@
+<script setup>
+import NavigationList from '../molecules/NavigationList.vue';
+</script>
+
 <template>
   <header class="header">
     <div class="header__container">
       <img class="header__logo" src="/assets/logo.png" alt="Logo Image" />
-      <nav class="header__nav">
-        <ul class="header__list">
-          <li class="header__item">
-            <svg class="header__icon">
-              <use href="#all" xlink-href="#all" />
-            </svg>
-          </li>
-          <li class="header__item">
-            <svg class="header__icon">
-              <use href="#movies" xlink-href="#movies" />
-            </svg>
-          </li>
-          <li class="header__item">
-            <svg class="header__icon">
-              <use href="#tv" xlink-href="#tv" />
-            </svg>
-          </li>
-          <li class="header__item">
-            <svg class="header__icon">
-              <use href="#bookmark" xlink-href="#bookmark" />
-            </svg>
-          </li>
-        </ul>
-      </nav>
-      <img class="header__user" src="/assets/user.png" alt="Logo Image" />
+      <navigation-list />
+      <img class="header__user" src="/assets/user.svg" alt="Logo Image" />
     </div>
   </header>
 </template>
@@ -73,49 +54,6 @@
     @media screen and (min-width: 768px) {
       height: 2.56rem;
       width: 3.2rem;
-    }
-  }
-
-  &__nav {
-    width: 100%;
-    height: 100%;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-
-    @media screen and (min-width: 1024px) {
-      align-items: flex-start;
-    }
-  }
-
-  &__list {
-    list-style: none;
-    display: flex;
-    align-items: center;
-    column-gap: 2.4rem;
-
-    @media screen and (min-width: 1024px) {
-      flex-direction: column;
-      row-gap: 4rem;
-    }
-  }
-
-  &__item {
-  }
-
-  &__icon {
-    width: 2rem;
-    height: 2rem;
-    fill: var(--color-blue-200);
-    cursor: pointer;
-    transition: all 0.3s;
-
-    &:hover {
-      fill: var(--color-white);
-    }
-
-    &--active {
-      fill: var(--color-white);
     }
   }
 
